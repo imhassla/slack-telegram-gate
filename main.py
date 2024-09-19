7b50eb9c0b86580daa42455826a678d878a5ea88
Daddy_0x <45751261+imhassla@users.noreply.github.com>
2024-09-19 17:14:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,262 @@
+from utils import download_file_from_slack, process_reply_message, find_project_by_chat_id, get_slack_username, find_project_by_slack_channel
+from utils import get_server_ip, start_config_monitor, process_reply_to_message, update_slack_thread_ts_by_string, save_thread_ts, load_config
+from flask import Flask, request, jsonify
+from slack_sdk import WebClient
+import subprocess
+import threading
+import telebot
+import logging
+import sqlite3
+import requests
+import signal
+import sys
+import os
+
+# Setting up logging
+logging.basicConfig(
+    filename='integration.log', 
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+current_config = load_config()
+conn = sqlite3.connect('messages.db', check_same_thread=False)
+cursor = conn.cursor()
+
+# Setting up a Telegram bot
+telegram_bot = telebot.TeleBot(current_config['bot_settings']['telegram_bot_gate_token'])
+logging.debug("Telegram bot configured")
+
+def send_text_to_slack(message, slack_client, project, sender_name, telegram_username):
+    thread_ts = process_reply_message(message, project)
+
+    slack_message_text = f"{sender_name} \n{telegram_username}\n\n{message.text}"
+    slack_response = slack_client.chat_postMessage(
+        channel=project['slack_channel_id'],
+        text=slack_message_text,
+        thread_ts=thread_ts  
+    )
+    logging.debug(f"Text message sent to Slack for a project {project['project_name']} with ts={slack_response['ts']}")
+
+    # Maintain correspondence between Telegram message_id and Slack ts
+    save_thread_ts(message.message_id,  slack_response['ts'], project['project_name'])
+    logging.debug(f"Matching message_id {message.message_id} and ts {slack_response['ts']} saved in database.")
+
+def send_media_to_slack(message, slack_client, project, sender_name, telegram_username):
+    thread_ts = process_reply_message(message, project)
+
+    # Get the correct file_id depending on the media message type
+    content_types = {
+        'photo': lambda msg: msg.photo[-1].file_id,
+        'document': lambda msg: msg.document.file_id,
+        'audio': lambda msg: msg.audio.file_id,
+        'video': lambda msg: msg.video.file_id,
+        'animation': lambda msg: msg.animation.file_id,
+        'voice': lambda msg: msg.voice.file_id 
+    }
+
+    file_id = content_types.get(message.content_type, lambda _: None)(message)
+    if not file_id:
+        logging.error(f"Unknown content type: {message.content_type}")
+        return
+
+    file_info = telegram_bot.get_file(file_id)
+    file_url = f'https://api.telegram.org/file/bot{current_config["bot_settings"]["telegram_bot_gate_token"]}/{file_info.file_path}'
+    filename = file_info.file_path.split('/')[-1]
+
+    file_response = requests.get(file_url)
+    if file_response.status_code == 200:
+        logging.debug(f"The file was successfully downloaded from Telegram: {file_url}")
+        slack_message_text = f"{sender_name} \n{telegram_username}"
+
+        slack_response = slack_client.files_upload_v2(
+            channel=project['slack_channel_id'],
+            file=file_response.content,
+            filename=filename,
+            title=message.caption or "Media from Telegram",
+            initial_comment=slack_message_text,
+            thread_ts=thread_ts
+        )
+
+        if slack_response["ok"]:
+            file_timestamp = slack_response['file']['id']
+            logging.debug(f"Media file sent to Slack for the project {project['project_name']} with ts={file_timestamp}")
+            save_thread_ts(message.message_id, file_timestamp, project['project_name'])
+        else:
+            logging.error(f"Error sending file to Slack: {slack_response['error']}")
+    else:
+        logging.error(f"Error downloading file from Telegram: status {file_response.status_code}, URL: {file_url}")
+
+# Slack Events API handler
+def process_slack_event(event):
+    if 'type' in event and event['type'] == 'message':
+        logging.debug(f"Handling a Slack event: event_type={event['type']}")
+        handle_slack_message(event)
+    else:
+        logging.debug(f"Skipping an irrelevant Slack event: {event['type']}")
+
+# Function for processing messages from Slack and forwarding them to Telegram
+def handle_slack_message(event):
+    logging.debug(f"Received message from Slack: channel_id={event['channel']}")
+    project = find_project_by_slack_channel(event['channel'])
+
+    update_slack_thread_ts_by_string(event, project)
+
+    if project:
+        slack_bot_member_id = project.get('slack_bot_member_id')
+        if event.get('user') == slack_bot_member_id:
+            logging.debug(f"Message sent by Slack bot {slack_bot_member_id}, skip forwarding.")
+            return
+
+    if project and project['active']:
+        slack_token = project['slack_bot_token']
+        slack_client = WebClient(token=slack_token)
+
+        slack_user_id = event.get('user')
+        slack_username = get_slack_username(slack_client, slack_user_id)
+        slack_user_id_tag = f"<@{slack_user_id}>"
+        
+        try:
+            reply_to_message_id, thread_ts = process_reply_to_message(event, project)
+
+            if 'files' in event:
+                for file in event['files']:
+                    telegram_response = send_file_to_telegram(
+                        file, slack_token, slack_username, slack_user_id_tag,
+                        event, project, reply_to_message_id
+                    )
+                    if telegram_response:
+                        save_thread_ts(telegram_response.message_id, thread_ts, project['project_name'])
+            else:
+                telegram_response = send_text_to_telegram(event, slack_username, slack_user_id_tag, project, reply_to_message_id)
+                save_thread_ts(telegram_response.message_id, thread_ts, project['project_name'])
+                return
+          
+        except Exception as e:
+            logging.error(f"Error when sending a message to Telegram for a project {project['project_name']}: {str(e)}")
+    else:
+        logging.warning(f"Project not found or not active for Slack channel_id={event['channel']}")
+
+# Processing messages in Telegram (text, photos, documents, audio, video, animations, voice messages)
+@telegram_bot.message_handler(content_types=['text','photo', 'document', 'audio', 'video', 'animation', 'voice'])
+def handle_media_message(message):
+    logging.debug(f"Processing a message from Telegram: chat_id={message.chat.id}, message_id={message.message_id}, type: {message.content_type}")
+    handle_telegram_message(message)
+
+def handle_telegram_message(message):
+    logging.debug(f"Received message from Telegram: chat_id={message.chat.id}, content_type={message.content_type}, message_id={message.message_id}")
+    project = find_project_by_chat_id(message.chat.id)
+    
+    if project and project['active']:
+        slack_token = project['slack_bot_token']
+        slack_client = WebClient(token=slack_token)
+        sender_name = message.from_user.full_name if message.from_user else "Unknown"
+        telegram_username = f"@{message.from_user.username}" if message.from_user.username else ""
+
+        try:
+            if message.content_type == 'text':
+                send_text_to_slack(message, slack_client, project, sender_name, telegram_username)
+            elif message.content_type in ['photo', 'document', 'audio', 'video', 'animation', 'voice']:
+                send_media_to_slack(message, slack_client, project, sender_name, telegram_username)
+            else:
+                logging.error(f"Content type error: {message.content_type}")
+        except Exception as e:
+            logging.error(f"Error when processing a message from Telegram: {str(e)}")
+
+def send_file_to_telegram(file, slack_token, slack_username, slack_user_id_tag, event, project, reply_to_message_id):
+    file_url = file['url_private']
+    local_file = download_file_from_slack(file_url, slack_token)
+    if local_file:
+        telegram_message = f"{slack_username} \n{slack_user_id_tag}"
+        if 'text' in event and event['text']:
+            telegram_message += f"\n\n{event['text']}"
+        
+        try:
+            with open(local_file, 'rb') as f:
+                telegram_response = telegram_bot.send_document(
+                    project['telegram_chat_id'],
+                    f,
+                    caption=telegram_message,
+                    reply_to_message_id=reply_to_message_id
+                )
+            logging.debug(f"The file was sent to Telegram for the project {project['project_name']}, message_id={telegram_response.message_id}")
+        except Exception as e:
+            logging.error(f"Error when sending a file to Telegram: {str(e)}")
+        finally:
+            os.remove(local_file)  
+        return telegram_response
+    else:
+        logging.error("The file was not uploaded from Slack.")
+    return None
+
+def send_text_to_telegram(event, slack_username, slack_user_id_tag, project, reply_to_message_id):
+    telegram_message = f"{slack_username} \n{slack_user_id_tag}\n\n{event['text']}"
+    telegram_response = telegram_bot.send_message(
+        project['telegram_chat_id'],
+        telegram_message,
+        reply_to_message_id=reply_to_message_id
+    )
+    logging.debug(f"Message sent to Telegram for the project {project['project_name']}, message_id={telegram_response.message_id}")
+    return telegram_response
+
+# Telegram bot in a separate thread
+def run_telegram_bot():
+    try:
+        logging.debug("Launching a Telegram bot survey")
+        while not stop_event.is_set():
+            telegram_bot.polling(none_stop=True, timeout=5) 
+    except Exception as e:
+        logging.critical(f"Critical error in Telegram bot: {str(e)}")
+    finally:
+        logging.info("Telegram bot stopped.")
+
+# Function to terminate the program correctly
+def signal_handler(sig, frame):
+    logging.info('Received SIGINT, shutting down...')
+    stop_event.set()  
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+app = Flask(__name__)
+# Route to receive events from Slack
+@app.route('/slack/events', methods=['POST'])
+def slack_event_handler():
+    data = request.json
+    
+    # Process challenge from Slack (during initial setup)
+    if 'challenge' in data:
+        return jsonify({'challenge': data['challenge']})
+    
+    # Immediately return the Slack response
+    if 'event' in data:
+        event_data = data['event']
+        threading.Thread(target=process_slack_event, args=(event_data,)).start()
+
+    return '', 200  
+
+if __name__ == '__main__':
+    # Event to stop threads
+    stop_event = threading.Event()
+    server_ip = get_server_ip()
+    print(f'http://{server_ip}:5000/slack/events')
+    logging.info(f"The server is running on {server_ip}. Use the URL for Slack Event Subscriptions: \nhttp://{server_ip}:5000/slack/events")
+
+    # Run Gunicorn in a separate process
+    gunicorn_command = ["gunicorn", "-w", "4", "-b", "127.0.0.1:5000", "main:app"]
+    gunicorn_process = subprocess.Popen(gunicorn_command)
+
+    # Run configuration monitoring in a separate thread
+    monitor_thread = start_config_monitor(interval=60)
+
+    # Run Telegram bot in a separate thread
+    telegram_thread = threading.Thread(target=run_telegram_bot)
+    telegram_thread.start()
+
+    # Wait for all threads to complete
+    telegram_thread.join()
+    monitor_thread.join()
+
+    # Terminate the Gunicorn process
+    gunicorn_process.terminate()
+    gunicorn_process.wait()
---
+++ utils.py
@@ -0,0 +1,269 @@
+import os
+import yaml
+import time
+import logging
+import sqlite3
+import requests
+import threading
+from queue import Queue
+from threading import Event
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+query_queue = Queue()
+queue_processed_event = Event()
+queue_processed_event.set()  
+config_last_loaded_time = 0  
+
+def worker():
+    global query_queue, queue_processed_event
+    while True:
+        time.sleep(3)  
+        if not query_queue.empty():
+            queue_processed_event.clear()  
+            while not query_queue.empty():
+                try:
+                    query, params = query_queue.get(timeout=1)
+                    cursor.execute(query, params)
+                    conn.commit()
+                    logging.debug(f"The request was completed with parameters: {params}")
+                    query_queue.task_done()
+                    
+                except sqlite3.Error as e:
+                    logging.error(f"Error executing request: {e}")
+            queue_processed_event.set()  
+
+threading.Thread(target=worker, daemon=True).start()
+lock = threading.Lock()
+
+conn = sqlite3.connect('messages.db', check_same_thread=False)
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS message_threads (
+    telegram_message_id INTEGER,
+    slack_thread_ts TEXT,
+    project_name TEXT,
+    PRIMARY KEY (telegram_message_id, project_name)
+)
+''')
+conn.commit()
+conn.close
+
+# Reply to slack from telegram
+def process_reply_message(message, project):
+    thread_ts = None
+    if message.reply_to_message:
+        logging.debug(f"The message is a reply to message_id={message.reply_to_message.message_id}")
+        thread_ts = get_thread_ts_from_slack(message.reply_to_message.message_id, project['project_name'])
+        if thread_ts:
+            logging.debug(f"thread_ts found for message_id={message.reply_to_message.message_id}: {thread_ts}")
+        else:
+            logging.warning(f"thread_ts not found for message with message_id={message.reply_to_message.message_id}")
+    return thread_ts
+
+# Search for a project by Slack channel
+def find_project_by_slack_channel(channel_id):
+    logging.debug(f"Search for a project for Slack channel_id={channel_id}")
+    for project in current_config['project_channels']:
+        if project['slack_channel_id'] == channel_id:
+            logging.debug(f"Project found: {project['project_name']}")
+            return project
+    logging.warning(f"Project not found for Slack channel_id={channel_id}")
+    return None
+
+# Search for a project by Telegram chat_id
+def find_project_by_chat_id(chat_id):
+    logging.debug(f"Search for a project for chat_id={chat_id}")
+    for project in current_config['project_channels']:
+        if project['telegram_chat_id'] == str(chat_id):
+            logging.debug(f"Project found: {project['project_name']}")
+            return project
+    logging.warning(f"Project not found for chat_id={chat_id}")
+    return None
+
+# Getting thread_ts from the database by message_id
+def get_thread_ts_from_slack(telegram_message_id, project_name):
+    cursor.execute('SELECT slack_thread_ts FROM message_threads WHERE telegram_message_id = ? AND project_name = ?', (telegram_message_id, project_name))
+    result = cursor.fetchone()
+    if result:
+        return result[0]  
+    return None
+
+# Getting Slack bot member ID
+def get_slack_bot_member_id(slack_bot_token):
+    try:
+        slack_client = WebClient(token=slack_bot_token)
+        response = slack_client.auth_test()  
+        slack_bot_member_id = response['user_id']
+        return slack_bot_member_id
+    except SlackApiError as e:
+        logging.error(f"Error running auth.test: {e.response['error']}")
+        return None
+
+# Start a background thread to monitor configuration changes
+def start_config_monitor(interval=60):
+    def monitor():
+        while True:
+            check_and_reload_config()
+            time.sleep(interval)
+    monitor_thread = threading.Thread(target=monitor)
+    monitor_thread.start()
+    logging.debug(f"Config change monitoring started")
+    return monitor_thread
+
+def check_and_reload_config():
+    global config_last_loaded_time
+    config_path = 'config.yaml'
+    try:
+        last_modified_time = os.path.getmtime(config_path)
+        # If the configuration has changed (by the time the file was changed)
+        if last_modified_time > config_last_loaded_time:
+            load_config()
+            # Update last download time
+            config_last_loaded_time = last_modified_time
+    except Exception as e:
+        logging.error(f"Error checking configuration: {str(e)}")
+
+# Function for loading configuration
+def load_config():
+    global current_config, config_last_loaded_time
+    try:
+        with open('config.yaml', 'r') as f:
+            new_config = yaml.safe_load(f)
+            current_config = new_config
+            config_last_loaded_time = time.time()
+            logging.debug("Configuration file updated.")
+            # Get and save slack_bot_member_id for each project
+            for project in current_config['project_channels']:
+                slack_token = project['slack_bot_token']
+                project['slack_bot_member_id'] = get_slack_bot_member_id(slack_token)
+            
+            return current_config
+    except Exception as e:
+        logging.error(f"Error loading configuration: {str(e)}")
+
+# Function to get external IP address
+def get_server_ip():
+    while True:
+        try:
+            ip_url = 'https://httpbin.org/ip'
+            response = requests.get(ip_url)
+            data = response.json()
+            sip = data.get('origin')
+            return sip
+        except:
+            time.sleep(5)
+
+# Function for downloading files from Slack
+def download_file_from_slack(file_url, slack_token):
+    headers = {
+        'Authorization': f'Bearer {slack_token}'
+    }   
+    
+    try:
+        file_response = requests.get(file_url, headers=headers, stream=True, timeout=60)
+        
+        if file_response.status_code == 200:
+            local_filename = file_url.split("/")[-1]
+            local_filepath = f'/tmp/{local_filename}'
+
+            logging.debug(f"Uploading a file from Slack. Expected content type: {file_response.headers.get('Content-Type')}")
+
+            with open(local_filepath, 'wb') as f:
+                for chunk in file_response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+
+            actual_file_size = os.path.getsize(local_filepath)
+            logging.debug(f"File {local_filename} successfully downloaded from Slack. Path: {local_filepath}, actual size: {actual_file_size}.")
+
+            return local_filepath
+        else:
+            logging.error(f"Error uploading file from Slack: {file_url}, status: {file_response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error uploading file from Slack: {str(e)}")
+        return None
+
+def update_slack_thread_ts_by_string(event, project):
+    logging.debug(f"Processing message for project_name={project['project_name']}, event_ts={event.get('event_ts')}, slack_thread_ts={event.get('slack_thread_ts')}")
+    time.sleep(4)
+    queue_processed_event.wait() 
+    try:
+        with lock:
+            if 'files' in event and len(event['files']) > 0:
+                file_info = event['files'][0]
+                timestamp = str(file_info.get('id', ''))
+            else:
+                logging.debug("Could not find file or timestamp in this 'event''")
+                return
+
+            event_ts = str(event.get('event_ts', ''))
+            if not event_ts:
+                logging.debug("Could not find event_ts in event")
+                return
+
+            project_name = project['project_name']
+
+            logging.debug(f"timestamp: {type(timestamp)}: {timestamp}")
+            logging.debug(f"event_ts: {type(event_ts)}: {event_ts}")
+            logging.debug(f"project_name: {type(project_name)}: {project_name}")
+
+            # Wait for all requests in the queue to be processed
+            queue_processed_event.wait() 
+            
+            # Search for an entry with timestamp and project_name in the database
+            cursor.execute('''
+            SELECT telegram_message_id FROM message_threads 
+            WHERE slack_thread_ts = ? AND project_name = ?
+            ''', (timestamp, project_name))
+            
+            result = cursor.fetchone()
+
+            if result:
+                telegram_message_id = result[0]
+                logging.debug(f"Found telegram_message_id: {telegram_message_id}")
+                save_thread_ts(telegram_message_id, event_ts, project_name)
+            else:
+                logging.warning(f"Entry with slack_thread_ts={timestamp} for project {project_name} was not found in the database.")
+                       
+    except Exception as e:
+        logging.error(f"Error updating message with slack_thread_ts={event.get('slack_thread_ts')}: {str(e)}")
+
+def save_thread_ts(telegram_message_id, slack_thread_ts, project_name):
+    try:
+        # Add an update request to the queue
+        query_queue.put((
+            '''
+            INSERT OR REPLACE INTO message_threads (telegram_message_id, slack_thread_ts, project_name)
+            VALUES (?, ?, ?)
+            ''', (telegram_message_id, slack_thread_ts, project_name)
+        ))
+    except ValueError as ve:
+        logging.error(f"Error while converting data: {ve}")
+
+def get_slack_username(slack_client, slack_user_id):
+    try:
+        # Get user information via Slack API
+        user_info = slack_client.users_info(user=slack_user_id)
+        # Username or real name
+        slack_username = user_info['user'].get('real_name') or user_info['user'].get('name')
+        return slack_username
+    except SlackApiError as e:
+        logging.error(f"Error getting Slack user information: {e.response['error']}")
+        return "Unknown User"
+    
+# Function for getting message_id from the database by thread_ts and project_name
+def get_telegram_message_id_by_thread_ts(thread_ts, project_name):
+    cursor.execute('SELECT telegram_message_id FROM message_threads WHERE slack_thread_ts = ? AND project_name = ?', (thread_ts, project_name))
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    return None
+
+def process_reply_to_message(event, project):
+    thread_ts = event.get('thread_ts') or event.get('ts')
+    reply_to_message_id = None
+    if 'thread_ts' in event:
+        reply_to_message_id = get_telegram_message_id_by_thread_ts(event['thread_ts'], project['project_name'])
+    return reply_to_message_id, thread_ts
